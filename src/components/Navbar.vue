bf9f8515d2347442fe7c4e68be70404e01b225e5
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-20 14:06:35 -0400

    navbar and routing in place

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand :to="{name: 'Home'}">Home</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item :to="{name: 'About'}">About Me</b-nav-item>
+        <b-nav-item>Portfolio</b-nav-item>
+        <b-nav-item>Contact</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
