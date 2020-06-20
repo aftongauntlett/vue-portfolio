49ed29d3476efc0375c2f25e1794d381b3cedf0b
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-06-20 14:08:59 -0400

    navbar styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="info">
+  <b-navbar toggleable="lg" type="dark" variant="dark">
     <b-navbar-brand :to="{name: 'Home'}">Home</b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -18,5 +18,5 @@
 export default {};
 </script>
 
-<style>
+<style scoped>
 </style>
