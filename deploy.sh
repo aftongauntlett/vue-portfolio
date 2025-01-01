5bbc9ed0380c73fb82359ddc91068b386301159d
afton <afton.gauntlett>
2025-01-01 18:28:25 -0500

    updating deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,10 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# force push to docs repo
-git push -f https://github.com/aftongauntlett/vue-portfolio-public.git master:gh-pages
+# Check if the remote origin exists and if not, add it
+git remote add origin https://github.com/aftongauntlett/vue-portfolio-public.git
+
+# Force push to gh-pages branch
+git push -f https://github.com/aftongauntlett/vue-portfolio-public.git main:gh-pages
 
 cd -
